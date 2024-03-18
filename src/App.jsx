1708917e8a8b80fd79cbcc76d1767be6038bcbec
jontyxo <joyesh16wa@gmail.com>
2024-03-18 20:17:36 +0530

    dfsdsdsd

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { useEffect } from 'react';
 import './App.css'
 
 function App() {
- 
+  useEffect(() => {
+    // Navigate to another link after component mounts
+    window.location.href = 'https://joyeshwadhwa.netlify.app/';
+  }, []); 
   return (
     <>
+    
         </>
   )
 }
